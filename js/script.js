eef431cb4b9933528302085d123bb0d506ca9938
chiaramery <meringolachiara@gmail.com>
2022-10-26 16:27:25 +0200

    Esercizio con click sul thumb e autoplay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+/* 
+    Slider con Vue
+    Bonus:
+    [*] 1- al click su una thumb, visualizzare in grande l'immagine corrispondente,
+    [*] 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente,
+    [] 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+*/
 const {createApp} = Vue;
-
 createApp ({
     data () {
         return {
@@ -24,28 +30,41 @@ createApp ({
                     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
                     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
                     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-                ]
+                ],
+                intervallo: null,
             },
             currentSlide: 0
         };
     },
     methods:{
-        showNext: function () {
-            if(this.currentSlide < this.carousel.images.lenght -1) {
+        showNext:function() {
+            if (this.currentSlide < this.carousel.images.length - 1) {
                 this.currentSlide++;
-            }else {
+            } else {
                 this.currentSlide = 0;
             }
         },
-        showPrev: function () {
-            if(this.currentSlide > 0 ){
+        showPrev: function() {
+            if ( this.currentSlide > 0) {
                 this.currentSlide--;
             } else {
-                this.currentSlide = this.carousel.images.lenght - 1;
+                this.currentSlide = this.carousel.images.length - 1;
             }
         },
         showClickedSlide (clickedSlide) {
-            this.currentSlide = click
-        }
+            this.currentSlide = clickedSlide;
+        },
+        intervalData() {
+            this.intervallo = setInterval(() => {
+                if (this.currentSlide < this.carousel.images.length - 1) {
+                    this.currentSlide++;
+                } else {
+                    this.currentSlide = 0;
+                }
+            },3000)
+        },
+    },
+    created () {
+        this.intervalData();
     }
 }).mount("#app");
